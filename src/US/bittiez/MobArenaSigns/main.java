8c8718a06918430a2a648193fb4a83360ec66e17
bittiez <bittiez@gmail.com>
2016-04-21 00:58:37 -0600

    Initial Commit

---
+++ src/US/bittiez/MobArenaSigns/main.java
@@ -0,0 +1,19 @@
+package US.bittiez.MobArenaSigns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by bittiez on 21/04/16.
+ */
+public class main extends JavaPlugin {
+    private static Logger log;
+
+    @Override
+    public void onEnable(){
+        log = getLogger();
+    }
+
+    
+}
